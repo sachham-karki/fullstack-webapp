73792a4255dc6ce5f9e2a084f136731727557153
SachKar <88773679+sachham-karki@users.noreply.github.com>
2022-08-26 23:21:51 +1000

    Backend setup

--- blog/src/pages/NotFoundPage.js
+++ blog/src/pages/NotFoundPage.js
@@ -1,4 +1,19 @@
 import React from "react";
 
-const NotFoundPage = ({ dog }) => <h1>404: {dog} Page Not Found</h1>;
+const NotFoundPage = ({ dog }) => (
+  <h1>
+    404 Error:{" "}
+    <span
+      style={{
+        color: "red",
+        fontSize: "60px",
+        fontStyle: "italic",
+        borderBottom: "dotted",
+      }}
+    >
+      {dog}
+    </span>{" "}
+    Page Not Found
+  </h1>
+);
 export default NotFoundPage;
