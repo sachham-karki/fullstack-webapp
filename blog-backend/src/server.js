141a0bb752c7ac817a215ffa3d421d10ccf23b43
SachKar <88773679+sachham-karki@users.noreply.github.com>
2022-08-27 16:45:33 +1000

    testing local data with http methods

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -5,9 +5,42 @@ const PORT = 8000;
 
 app.use(express.json());
 
-app.get("/hello", (req, res) => res.send("Hello!"));
+const articlesInfo = {
+  "learn-react": {
+    upvotes: 0,
+    comments: [],
+  },
+  "learn-node": {
+    upvotes: 0,
+    comments: [],
+  },
+  "my-thoughts-on-resumes": {
+    upvotes: 0,
+    comments: [],
+  },
+};
 
-app.post("/hell", (req, res) => res.send(`Hello ${req.body.name}`));
+//add comments end point
+app.post("/api/articles/:name/add-comment", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].comments.push({ username, text });
+
+  res.status(200).send(articlesInfo[articleName]);
+});
+
+//up voting end point
+app.post("/api/articles/:name/upvote", (req, res) => {
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].upvotes += 1;
+  res
+    .status(200)
+    .send(
+      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`
+    );
+});
 
 app.listen(8000, () => {
   console.log(`Port is listen at ${PORT}!`);
